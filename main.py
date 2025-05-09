62967136f8feb90e3481d308c47b1157c265baf3
listwas <listwa.patrykkr@gmail.com>
2025-05-09 23:25:00 +0200

    csv file load&write

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
-
+import csv, os
 
 class Library:
 
     def __init__(self):
         self.books = []
+        self.args = ['title', 'author', 'pages', 'status']
+        self.file_name = 'books_data.csv'
+        self.read_file()
+        
+
+    def read_file(self):
+        with open(self.file_name, 'r') as csvfile:
+            cr = csv.reader(csvfile, delimiter='|')
+
+            for row in cr:
+                book = {}
+                index = 0    
+                for item in self.args:
+                    index += 1
+                    book[item] = row[index-1]
+                self.books.append(book)
+
 
     def add_book(self):
         print('\nadd your book')
-        args = ['title', 'author', 'pages', 'status']
 
         book = {}
-        for item in args:
+        for item in self.args:
             book[item] = input(f'{item}: ')
 
         self.books.append(book)
+
+        # save data to file 
+        with open(self.file_name, 'w') as file:
+            w = csv.writer(file, delimiter='|')
+            w.writerows([x.values() for x in self.books])
         
     def method_used(self, x):
         match x:
             case 'a':
                 self.add_book()
             case 'l':
-                print(*self.books, sep='\n')
+                print(self.books)
             case 'r':
                 """ remove book """
 
@@ -34,7 +55,7 @@ def main():
             'a - add book\n'
             'l - list all books\n'
             'r - remove book\n'
-        )
+        ).lower()
         lib.method_used(choice)
 
         
