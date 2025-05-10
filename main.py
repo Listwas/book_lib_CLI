67f8f32c735e8a33d7d643f4f0e45b3088c9566e
listwas <listwa.patrykkr@gmail.com>
2025-05-10 15:20:57 +0200

    cleaning, text formatting

--- main.py
+++ main.py
@@ -1,39 +1,51 @@
 import json, os
 
+BOOK_STATUSES = {
+    'w' : 'want to read',
+    'r' : 'reading',
+    'c' : 'completed'
+}
+
+MENU_OPTIONS = {
+    'a' : 'add books',
+    'l' : 'list all books',
+    'r' : 'remove book',
+    'e' : 'exit'
+}
+
+
 class Library:
 
     def __init__(self):
         self.books = []
         self.args = ['title', 'author', 'pages', 'status']
         self.file_name = 'books_data.json'
+        self.end = ''
 
-        # create file if one doesn't exist, then load book data
         if not os.path.exists(self.file_name):
             with open(self.file_name, 'w') as file:
                 json.dump([], file)
 
         self.load_books_data()
-        self.end = ''
-                
         self.clear_cli()
 
     def clear_cli(self):
-        if os.name == 'nt':
-            os.system('cls')
-        else: 
-            os.system('clear')
+        os.system('cls' if os.name == 'nt' else 'clear')
 
     # load file contents and store them in a book list 
     def load_books_data(self):
         if os.path.getsize(self.file_name) > 0:
             with open(self.file_name, 'r') as file:
-                data = json.load(file)
-                self.books.extend(data)
+                self.books = json.load(file)
     
+    def save_books(self):
+        with open(self.file_name, 'w') as file:
+            json.dump(self.books, file, indent=4)
+
     # determinate which metod user selected
-    def method_used(self, x):
+    def method_used(self, option):
         self.clear_cli()
-        match x:
+        match option:
             case 'a':
                 print('add your book')
                 self.add_book()
@@ -47,48 +59,33 @@ class Library:
     # adding books to the JSON file and list of books
     def add_book(self):
         book = {}
+
         for key in self.args:
-            if not key == 'status':
-                data = input(f'{key}: ')
+            if key == 'status':
+                book[key] = self.get_book_status()
+                
             else:
-                data = self.get_book_status()
-                self.clear_cli()
-                print(f'book {book['title']} added')
-
-            book[key] = data
+                book[key] = input(f'{key}: ')\
+            
         self.books.append(book)
+        self.save_books()
 
-        with open(self.file_name, 'w') as file:
-            json.dump(self.books, file, indent=4)
+        self.clear_cli()    
+        print(f'book {book['title']} added')
      
     def get_book_status(self):
         self.clear_cli()
-        print(
-                'w - want to read\n'
-                'r - currently reading\n'
-                'c - completed\n'
-            )
-
-        status = input('status: ')
-        match status:
-            case 'w':
-                return 'want to read'
-            case 'r':
-                return 'reading'
-            case 'c':
-                return 'completed'
-            case _:
-                return 'not specified'
+        print('\n'.join([f'{key} - {value}' for key, value in BOOK_STATUSES.items()]))
+        return BOOK_STATUSES.get(input('status: '), 'not specified')
 
     # list all books
     def list_books(self):
         if not self.books:
             print('no books to display')
-            return
-        
-        for item in self.books:
-            print(json.dumps(item, indent = 4))
-        
+        else:    
+            for book in self.books:
+                print(json.dumps(book, indent = 4))
+            
         input('press enter to exit')
         self.clear_cli()
 
@@ -102,16 +99,15 @@ class Library:
         for i, book in enumerate(self.books, 1):
             print(f'{i}. {book['title']} by {book['author']}')    
 
-        key = input('enter book title to delete\ntitle: ')
+        title = input('enter book title to delete\ntitle: ')
 
-        new_books = [book for book in self.books if book.get('title') != key]
-        
+        new_books = [book for book in self.books if book.get('title') != title]
+
+        self.clear_cli()        
         if len(new_books) < len(self.books):
             self.books = new_books
-            with open(self.file_name, 'w') as file:
-                json.dump(self.books, file, indent=4)
-            self.clear_cli()
-            print(f'removed book: {key}')
+            self.save_books()
+            print(f'removed book: {title}')
         else:
             print('book not found')
           
@@ -119,18 +115,11 @@ def main():
     lib = Library()
 
     while lib.end != 'e':
-        choice = input(
-            'a - add book\n'
-            'l - list all books\n'
-            'r - remove book\n'
-            'e - exit\n'
-        ).lower()
+        choice = input('\n'.join([f'{key} - {value}'
+                       for key, value in MENU_OPTIONS.items()]) + '\n').lower()
         lib.method_used(choice)
 
         
 
 main()
 
-# TODO
-# remove book from list and file
-# text formatting
