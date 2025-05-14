f0c3ecc3db012bf23a289570f8647ed89a7685b4
listwas <listwa.patrykkr@gmail.com>
2025-05-14 16:27:03 +0200

    getter for title

--- app/server.py
+++ app/server.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request, redirect
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def menu_page():
-    return render_template("index.html")
+class Routing:    
+    @app.route("/")
+    def menu_page():
+        title = request.args.get('added')
+        msg = f"book <span>{title}</span> added" if title else ""
+        return render_template("index.html", msg=msg)
 
-@app.route("/add_book")
-def add_book():
-    return render_template("add_book.html")
+
+    @app.route("/add_book")
+    def add_book():
+        return render_template("add_book.html")
+
+
+    @app.route("/get_data", methods=['POST'])
+    def get_data():
+        title = request.form['title']
+        return redirect(f"/?added={title}")
+
+
+
 app.run()
